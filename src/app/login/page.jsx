cba84caa20e63f92b9cc09705075820022418133
mahmudul-Hasan-2 <mahmudul5709@gmail.com>
2026-05-01 13:48:36 +0600

    Fixed the metadata spacing

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,12 +3,10 @@ import LoginPage from "@/Components/authentication/LoginPage";
 export const metadata = {
   title: "Qurbani Hat - Login",
   description:
-    "Securely log in to your Qurbani Hat account to manage your livestock bookings, track your animal's delivery, and access your saved favorites. Join Bangladesh's most trusted online Qurbani marketplace.",
+    "Securely log in to your Qurbani Hat account to manage your livestock bookings, track your animal's delivery, and access your saved favorites. Join Bangladesh's most trusted online Qurbani marketplace. ",
 };
 const Login = () => {
-  return (
-      <LoginPage></LoginPage>
-  );
+  return <LoginPage></LoginPage>;
 };
 
 export default Login;
